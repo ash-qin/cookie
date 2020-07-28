4eeb29ca2253e541049d6920b247bb9e1d9d9938
ash-qin <ash-qin@maple.com>
2020-07-28 16:58:00 +0800

    commit first 2020/7/26 20:31

---
+++ src/com/qin/ash/CookeDemo.java
@@ -0,0 +1,56 @@
+package com.qin.ash;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/CookeDemo")
+public class CookeDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Cookie[] cookies = request.getCookies();
+        response.setContentType("text/html;charset=UTF-8");
+
+        Cookie cookie1 = null;
+        for (Cookie cookie : cookies) {
+
+            if ("lastvisit".equals(cookie.getName())){
+                cookie1 = cookie;
+                break;
+            }
+
+        }
+
+        if (cookie1 != null){
+
+            response.getWriter().print("您的上一次访问时间为：" + URLDecoder.decode(cookie1.getValue()));
+            response.getWriter().write("您的上一次访问时间为：" + cookie1.getValue());
+
+        }else{
+
+            response.getWriter().print("您是第一次访问本网站");
+
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss-SSS");
+
+        String format = sdf.format(new Date());
+        format = URLEncoder.encode(format);
+
+        Cookie cookie = new Cookie("lastvisit",format);
+        response.addCookie(cookie);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
